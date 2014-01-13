bf84ed9957dc48f2872c02b4ef02cadadf7255d6
Raúl Ogaz <raul260290@gmail.com>
2014-01-13 16:12:45 -0700

    Se crean scaffolds de Company y Branch.

---
+++ app/models/branch.rb
@@ -0,0 +1,5 @@
+class Branch < ActiveRecord::Base
+
+  belongs_to :company
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cyberclock::Application.routes.draw do
+  resources :branches
+
+  resources :companies
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
