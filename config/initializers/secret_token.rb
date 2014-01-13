2ee1753cfb9cf715db4f21f456fd195ef76c8dec
Raúl Ogaz <raul260290@gmail.com>
2014-01-13 10:50:04 -0700

    Commit inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyberclock::Application.config.secret_key_base = 'fa3921d82b34f169972a990f74fa6c4badda82b0613d3437bb2414ad30dd4d5e0222125dfdae2faa396128730996f93e067aefe0e05e561dfb56342305d96efb'
