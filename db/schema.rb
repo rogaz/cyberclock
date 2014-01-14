1372b0c6880c781949bac247123fa1a9d73ae771
Rolando Iván Vázquez Hernández <rivazquez@imac-de-rolando.ti.uach.mx>
2014-01-14 14:47:45 -0700

    Se ajusta posicion de logout en application.html. Se deja en el dropdown.

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140114041442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
-  create_table "user_sessions", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-create_table "branches", force: true do |t|
+  create_table "branches", force: true do |t|
     t.string   "name"
     t.integer  "company_id"
     t.datetime "created_at"
@@ -46,28 +39,6 @@ create_table "branches", force: true do |t|
   end
 
   add_index "computers", ["branch_id"], name: "index_computers_on_branch_id", using: :btree
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "phone"
-    t.string   "login",                           null: false
-    t.string   "email",                           null: false
-    t.string   "crypted_password",                null: false
-    t.string   "password_salt",                   null: false
-    t.string   "persistence_token",               null: false
-    t.string   "single_access_token",             null: false
-    t.string   "perishable_token",                null: false
-    t.integer  "login_count",         default: 0, null: false
-    t.integer  "failed_login_count",  default: 0, null: false
-    t.datetime "last_request_at"
-    t.datetime "current_login_at"
-    t.datetime "last_login_at"
-    t.string   "current_login_ip"
-    t.string   "last_login_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "promotions", force: true do |t|
     t.string   "description"
@@ -97,5 +68,32 @@ create_table "branches", force: true do |t|
   end
 
   add_index "rules", ["company_id"], name: "index_rules_on_company_id", using: :btree
+
+  create_table "user_sessions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "login",                           null: false
+    t.string   "email",                           null: false
+    t.string   "crypted_password",                null: false
+    t.string   "password_salt",                   null: false
+    t.string   "persistence_token",               null: false
+    t.string   "single_access_token",             null: false
+    t.string   "perishable_token",                null: false
+    t.integer  "login_count",         default: 0, null: false
+    t.integer  "failed_login_count",  default: 0, null: false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
