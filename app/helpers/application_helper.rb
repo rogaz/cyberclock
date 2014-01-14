014b13b9637613bebb56dad1eae8905d3552db85
Raúl Ogaz <raul260290@gmail.com>
2014-01-13 22:15:17 -0700

    Permite crear sucursales para una empresa.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def validate_if_nil(object_envoy, methods_for_object)
+    all_methods = methods_for_object.split('.')
+    string_of_methods = ""
+    flag = false
+
+    all_methods.each do |method|
+      string_of_methods += ".send('#{method.to_s}')"
+      if eval("object_envoy#{string_of_methods}.nil?")
+        flag = true
+        break
+      else
+        flag = false
+      end
+    end
+
+    unless flag
+      eval("object_envoy#{string_of_methods}")
+    else
+      'No disponible'
+    end
+  end
+
 end
