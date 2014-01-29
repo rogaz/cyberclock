309c0d0f8d831acb27a22ee0db6aee0ee3a81abd
Raúl Ogaz <raul260290@gmail.com>
2014-01-28 19:19:21 -0700

    Se eliminan columnas de migración para attachment de picture, porque se agregan con t.attachment :picture.

--- db/migrate/20140115190841_create_products.rb
+++ db/migrate/20140115190841_create_products.rb
@@ -3,11 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.text :description
       t.references :company, index: true
-      t.string   "picture_file_name"
-      t.string   "picture_content_type"
-      t.integer  "picture_file_size"
-      t.datetime "picture_updated_at"
-
+      
       t.timestamps
     end
   end
